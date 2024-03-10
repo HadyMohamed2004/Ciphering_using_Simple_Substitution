41b3778f8fdadc19e5d5102cd3235d838dd87c0b
Hady Mohammed <hady.m.meawad@gmail.com>
2024-03-10 17:38:40 +0200

    10.3

--- main.cpp
+++ main.cpp
@@ -4,39 +4,73 @@
 using namespace std;
 
 
-int main() {
-
-    string alpha = "abcdefghijklmnopqrstuvwxyz";
+string alpha = "abcdefghijklmnopqrstuvwxyz";
 
 
-    string ciphering, str, key;
+string create_key() {
+    string ciphering, key;
     cout << "Enter Key: " << endl;
     getline(cin, key);
-    ciphering = key;
 
-
-    cout << "Enter A Sentence to Cipher:";
-    getline(cin, str);
     for (auto &x: key) {
         x = tolower(x);
     }
 
+    ciphering = key;
     for (int i = 0; i < 26; i++) {
         if (not key.contains(alpha[i])) {
             ciphering += alpha[i];
         }
     }
-    string newline;
+    return ciphering;
+}
 
-    //ciphering
-    for (int j = 0; j < str.size(); j++) {
-        if (isalpha(str[j])) {
-            newline += ciphering[alpha.find(str[j])];
-        }
-        else{
-            newline += str[j];
+
+void simple_sub_cipher() {
+
+    string ciphering = create_key(), str, newline;
+
+    cout << "Enter A Sentence to Cipher:";
+    getline(cin, str);
+
+
+
+//    cipher
+    for (char j: str) {
+        j = tolower(j);
+        if (isalpha(j)) {
+            newline += ciphering[alpha.find(j)];
+        } else {
+            newline += j;
         }
     }
+    cout << newline << endl;
+}
+
+
+void simple_sub_decipher() {
+
+    string ciphering = create_key(), str, newline;
+
+    cout << "Enter A Sentence to Cipher:";
+    getline(cin, str);
+
+
+    for (char j: str) {
+        j = tolower(j);
+        if (isalpha(j)) {
+            newline += alpha[ciphering.find(j)];
+        } else {
+            newline += j;
+        }
+    }
+    cout << newline << endl;
+}
+
+
+int main() {
+
+    simple_sub_decipher();
 
     return 0;
 }
